515e22a39a998701f3005b61a19bdc7d3bfc5085
Basilije <vasske@gmail.com>
2019-09-25 19:01:16 -0700

    after new video

--- MatchingPairsGame/MatchingPairsGame/Form1.cs
+++ MatchingPairsGame/MatchingPairsGame/Form1.cs
@@ -26,6 +26,8 @@ namespace MatchingPairsGame
             "b", "b", "v", "v", "w", "w", "z", "z"
         };
 
+        Label firstClicked, secondClicked;
+
         public FormMatchingPairsGame()
         {
             InitializeComponent();
@@ -47,5 +49,77 @@ namespace MatchingPairsGame
                 }
             }
         }
+
+
+
+        private void labelClick(object sender, EventArgs e)
+        {
+            if (firstClicked != null && secondClicked != null)
+                return;
+
+            Label clickedLabel = sender as Label;
+
+            if (clickedLabel == null)
+                return;
+
+            if (clickedLabel.ForeColor == Color.Black)
+                return;
+
+            if (firstClicked == null)
+            {
+                firstClicked = clickedLabel;
+                firstClicked.ForeColor = Color.Black;
+                return;
+            }
+
+            secondClicked = clickedLabel;
+            secondClicked.ForeColor = Color.Black;
+
+            CheckForWinner();
+
+            if (firstClicked.Text == secondClicked.Text)
+            {
+                firstClicked = null;
+                secondClicked = null;
+            }
+            else
+                timer1.Start();
+        }
+
+ 
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+            firstClicked.ForeColor = firstClicked.BackColor;
+
+            secondClicked.ForeColor = secondClicked.BackColor;
+
+            firstClicked = null;
+            secondClicked = null;
+
+        }
+
+
+        private void CheckForWinner()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label label = control as Label;
+
+     //           Label label;
+     //       for (int i=0; i<tableLayoutPanel1.Controls.Count; i++)
+     //       {
+     //           label = tableLayoutPanel1.Controls[i] as Label;
+
+                if (label != null && label.ForeColor == label.BackColor)
+                    return;
+            }
+
+            MessageBox.Show("You matched all the icons! Congrats!");
+            Close();
+        }
+
     }
 }
