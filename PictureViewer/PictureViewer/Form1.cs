154d45a44be24ebee290de61150cdcd845888cab
Basilije <vasske@gmail.com>
2019-10-16 19:21:22 -0700

    After Video #7 (Write code for additonal buttons and checkbox).

--- PictureViewer/PictureViewer/Form1.cs
+++ PictureViewer/PictureViewer/Form1.cs
@@ -19,17 +19,18 @@ namespace PictureViewer
 
         private void CloseButton_Click(object sender, EventArgs e)
         {
-
+            this.Close();
         }
 
         private void BackgroundButton_Click(object sender, EventArgs e)
         {
-
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            pictureBox1.BackColor = colorDialog1.Color;
         }
 
         private void ClearButton_Click(object sender, EventArgs e)
         {
-
+            pictureBox1.Image = null;
         }
 
         private void ShowButton_Click(object sender, EventArgs e)
@@ -38,6 +39,14 @@ namespace PictureViewer
             {
                 pictureBox1.Load(openFileDialog1.FileName);
             }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            else
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
         }
     }
 }
