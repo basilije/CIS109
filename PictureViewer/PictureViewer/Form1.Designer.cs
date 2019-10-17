8a55b3810d2f11d8314f223fc526240775f3d7b9
Basilije <vasske@gmail.com>
2019-10-16 19:30:23 -0700

    After Video #8 (76. Commenting your code).

--- PictureViewer/PictureViewer/Form1.Designer.cs
+++ PictureViewer/PictureViewer/Form1.Designer.cs
@@ -80,7 +80,7 @@
             this.checkBox1.TabIndex = 1;
             this.checkBox1.Text = "Stretch";
             this.checkBox1.UseVisualStyleBackColor = true;
-            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
             // 
             // flowLayoutPanel1
             // 
--- PictureViewer/PictureViewer/Form1.cs
+++ PictureViewer/PictureViewer/Form1.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/* Vasilije Mehandzic
+ * Reno Technology Academy
+ * CIS109: Coding in C# for Imbedded Systems
+ * Udemy, 17 Beginner C# Walktrough Projects Step By Step
+ * Project 5: Create a Picture Viewer
+ * */
+
+using System;
 using System.Windows.Forms;
 
 namespace PictureViewer
@@ -19,30 +19,38 @@ namespace PictureViewer
 
         private void CloseButton_Click(object sender, EventArgs e)
         {
+            // Close the Form.
             this.Close();
         }
 
         private void BackgroundButton_Click(object sender, EventArgs e)
         {
+            // Show the color dialog box. If the user clicks OK, change the
+            // PictureBox control's background to the color user chose.
             if (colorDialog1.ShowDialog() == DialogResult.OK)
-            pictureBox1.BackColor = colorDialog1.Color;
+                pictureBox1.BackColor = colorDialog1.Color;
         }
 
         private void ClearButton_Click(object sender, EventArgs e)
         {
+            // Clear the picture.
             pictureBox1.Image = null;
         }
 
         private void ShowButton_Click(object sender, EventArgs e)
         {
+            // Show the Open File dialog. If the user click OK, load the
+            // picture that user chose.
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
-            {
                 pictureBox1.Load(openFileDialog1.FileName);
-            }
         }
 
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
         {
+            // If the user selects the Stretch check box
+            // change the PictureBox's
+            // SizeMode property to "Stretch". If the user clears
+            // the check box, change it to "Normal".
             if (checkBox1.Checked)
                 pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             else
